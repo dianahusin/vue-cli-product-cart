f32d114d7727edcb89977125485ce5b33d244901
dianahusin <diana@cara.com.my>
2022-06-29 15:26:08 +0800

    Add to cart

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,69 @@
 <template>
- <header class="top-bar spread">
-        <nav class="top-bar-nav">
-          <router-link to="/" class="top-bar-link">
-            <i class="icofont-spoon-and-fork"></i>
-            <span>Home</span>
-          </router-link>
-          <router-link to="/products-view" class="top-bar-link">
-            <span>Products</span>
-          </router-link>
-          <router-link to="/past-orders" class="top-bar-link">
-            <span>Past Orders</span>
-          </router-link>
-        </nav>
-        <!-- <router-link class="top-bar-cart-link" @click="toggleSidebar">
-          <i class="icofont-cart-alt icofont-1x"></i>
-          <span>Cart ({{ totalQuantity }})</span>
-        </router-link> -->
-      </header>
-  <router-view/>
+  <header class="top-bar spread">
+    <nav class="top-bar-nav">
+      <router-link to="/" class="top-bar-link">
+        <i class="icofont-spoon-and-fork"></i>
+        <span>Home</span>
+      </router-link>
+      <router-link to="/products-view" class="top-bar-link">
+        <span>Products</span>
+      </router-link>
+      <router-link to="/past-orders" class="top-bar-link">
+        <span>Past Orders</span>
+      </router-link>
+    </nav>
+    <div class="top-bar-cart-link" @click="toggleSidebar">
+      <i class="icofont-cart-alt icofont-1x"></i>
+      <span>Cart ({{ totalQuantity }})</span>
+    </div>
+  </header>
+  <router-view
+    :inventory="inventory"
+    :addToCart="addToCart"
+  />
+
+  <SidebarView
+    v-if="showSidebar"
+    :toggle="toggleSidebar"
+    :cart="cart"
+    :inventory="inventory"
+    :remove="removeItem"
+  />
 </template>
 
-<style lang="scss">
+<script>
+import SidebarView from '@/components/SidebarView.vue'
+import food from './food.json'
 
-</style>
+export default {
+  components: {
+    SidebarView
+  },
+  data () {
+    return {
+      showSidebar: false,
+      inventory: food,
+      cart: {}
+    }
+  },
+  computed: {
+    totalQuantity () {
+      return Object.values(this.cart).reduce((acc, curr) => {
+        return acc + curr
+      }, 0)
+    }
+  },
+  methods: {
+    addToCart (name, quantity) {
+      if (!this.cart[name]) this.cart[name] = 0
+      this.cart[name] += quantity
+    },
+    toggleSidebar () {
+      this.showSidebar = !this.showSidebar
+    },
+    removeItem (name) {
+      delete this.cart[name]
+    }
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,62 +4,31 @@
       <div class="splash">
         <h1>Splendid Food</h1>
       </div>
+    </div>
+    <main class="wrapper">
+      <h2>Recommended</h2>
+      <div class="recommended">
+        <ProductCard
+          v-for="(product, index) in inventory.slice(0, 3)"
+          :key="product.id"
+          class="card"
+          :index="index"
+          :product="product"
+          :addToCart="addToCart"
+        />
       </div>
-      <main class="wrapper">
-        <h2>Recommended</h2>
-        <div class="recommended">
-          <div v-for="(product, i) in inventory.slice(0, 3)" :key="i" class="card">
-            <div class="card-title">{{ product.name }}</div>
-            <div class="card-body">
-              <i class="icofont-10x icofont-{{ product.icon }}"></i>
-              <form>
-                <div class="row">
-                  <div class="cell">
-                    <label>Type:</label>
-                  </div>
-                  <div class="cell">
-                    <em>{{ product.type }}</em>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="cell">
-                    <label>Price:</label>
-                  </div>
-                  <div class="cell">${{ product.price.USD.toFixed(2) }}</div>
-                </div>
-                <div class="row">
-                  <div class="cell">
-                    <label>Quantity:</label>
-                  </div>
-                  <div class="cell">
-                    <input type="number" v-model.number="product.quantity" min="0"/>
-                  </div>
-                </div>
-              </form>
-            </div>
-            <div class="card-footer">
-              <button @click="addToCart(product.name, i)" class="btn btn-light">
-                Add to cart
-              </button>
-            </div>
-          </div>
-
-        </div>
-      </main>
+    </main>
   </div>
 </template>
 
 <script>
-import food from '../food.json'
+import ProductCard from '@/components/ProductCard.vue'
 
 export default {
-  name: 'HomeView',
-  data () {
-    return {
-      inventory: food
-    }
-  },
   components: {
-  }
+    ProductCard
+  },
+  name: 'HomeView',
+  props: ['inventory', 'addToCart']
 }
 </script>
